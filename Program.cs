b80e4607abd4509e30fed413a9dfd56ec095587b
Robert4582 <43041342+Robert4582@users.noreply.github.com>
2022-01-05 12:26:36 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StorEntrypoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
